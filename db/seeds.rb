7f265bf2c848f5f7d521b1a32e03f69a7e80f90d
JanKarlicej <honza.karlicek@centrum.cz>
2015-07-27 15:58:17 +0200

    Add db a seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+contracts = Contract.create([["contract_num", ""], ["order_identifier", "AxA98s6s5d486e"], ["
+review_date", "2015-07-16 00:00:00.000000"], ["sample_income_date", "2015-07-16
+00:00:00.000000"], ["protocole_done_date", "2015-07-09 00:00:00.000000"], ["agre
+_finish_date", "2015-07-10 00:00:00.000000"], ["cost", 150000.0], ["backup_cost"
+, 150000.0], ["back_pay", 150000.0], ["fact_netto", 25000.0], ["customer_id", 3]
+, ["constract_type_id", 6], ["user_id", 3], ["created_at", "2015-07-27 13:52:29.
+761973"], ["updated_at", "2015-07-27 13:52:29.761973"]])
